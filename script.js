1c2e42d136028e7d5594ed93d8d407c9cfbf918e
thecodingloft <thecodingloft@gmail.com>
2022-03-15 16:48:54 +0100

    first-commit

--- script.js
+++ script.js
@@ -1,66 +1,40 @@
-var slideIndex = 1;
-var testimonials = document.querySelectorAll('.testimonial-card')
-var prev = document.querySelector('.prev')
-var next = document.querySelector('.next')
-var myTimeout 
+const swiper = new Swiper('.chef-slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    speed: 400,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
 
-showSlides(slideIndex);
-timer();
-
-function plusSlides(n) {
-    console.log(slideIndex)
-    showSlides(slideIndex += 1);
-    console.log(slideIndex)
-}
-
-function currentSlide(n) {
-    clearTimeout(myTimeout);
-    showSlides(slideIndex = n);
-    timer();
-}
-
-function showSlides(n) {
-    var i;
-    var testimonials = document.querySelectorAll('.testimonial-card')
-    var dots = document.querySelectorAll('.dot')
-
-    if (n > testimonials.length) {
-        slideIndex = 1;}
-    if (n < 1) {
-        slideIndex = testimonials.length;
-    }
-    for (i=0; i < testimonials.length; i++) {
-        testimonials[i].style.display = "none"
-    }
-    for (i=0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(' active', '')
-    }
-    testimonials[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active"
-} 
-
-function timer() {
-    myTimeout = setInterval(() => {
-        if (slideIndex < testimonials.length) {
-            showSlides(slideIndex++)
-        } else {
-            slideIndex = 1;
-            showSlides(slideIndex)
+    // responsive breakpoints:
+    breakpoints: {
+        300: {
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        800: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1400: {
+            slidesPerView: 5,
+            spaceBetween: 30
+        },
+        1800: {
+            slidesPerView: 7,
+            spaceBetween: 30
         }
-    }, 3000)
-}
+    },
 
+    // navigation
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
 
-prev.addEventListener('click', () => {
-    clearTimeout(myTimeout);
-    slideIndex = slideIndex - 1;
-    showSlides(slideIndex);
-    timer()
-})
-
-next.addEventListener('click', () => {
-    clearTimeout(myTimeout);
-    slideIndex = slideIndex + 1;
-    showSlides(slideIndex);
-    timer()
+    // pagination
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
 })
